6baffaf87e685fe88cd9c0543ef69e8c53f9531c
Matt Porritt <mattp@catalyst-au.net>
2020-01-19 21:21:20 +1100

    WIP: JS to create calendar

--- amd/src/main.js
+++ amd/src/main.js
@@ -27,6 +27,72 @@ define(['jquery'], function($) {
      * Module level variables.
      */
     var Main = {};
+    var today = new Date();
+    var currentMonth = today.getMonth();
+    var currentYear = today.getFullYear();
+
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    /**
+     * Check how many days in a month code
+     * from https://dzone.com/articles/determining-number-days-month.
+     */
+    function daysInMonth(month, year) {
+        return 32 - new Date(year, month, 32).getDate();
+    }
+
+    /**
+     * Generate calendar markup for the month.
+     */
+    function generateCalendar(month, year) {
+
+        let firstDay = (new Date(year, month)).getDay();  // Get the starting day of the month.
+
+        tbl = document.getElementById("calendar-body"); // Body of the calendar.
+
+        // Clearing all previous cells.
+        tbl.innerHTML = "";
+
+        // Filing data about month and in the page via DOM.
+        monthAndYear.innerHTML = months[month] + " " + year;
+        selectYear.value = year;
+        selectMonth.value = month;
+
+        // Creating all cells.
+        let date = 1;
+        for (let i = 0; i < 6; i++) {
+            // Creates a table row.
+            let row = document.createElement("tr");
+
+            // Creating individual cells, filing them up with data.
+            for (let j = 0; j < 7; j++) {
+                if (i === 0 && j < firstDay) {
+                    cell = document.createElement("td");
+                    cellText = document.createTextNode("");
+                    cell.appendChild(cellText);
+                    row.appendChild(cell);
+                }
+                else if (date > daysInMonth(month, year)) {
+                    break;
+                }
+
+                else {
+                    cell = document.createElement("td");
+                    cellText = document.createTextNode(date);
+                    if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
+                        cell.classList.add("bg-info");
+                    } // Color today's date.
+                    cell.appendChild(cellText);
+                    row.appendChild(cell);
+                    date++;
+                }
+
+            }
+
+            tbl.appendChild(row); // Appending each row into calendar body.
+        }
+
+    }
 
     /**
      * Initialise all of the modules for the overview block.
